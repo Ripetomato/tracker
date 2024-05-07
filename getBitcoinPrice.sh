a6228c424549ed293d662c7537afae549a1a67cf
Ripetomato <169016177+Ripetomato@users.noreply.github.com>
2024-05-07 22:11:00 +0800

    Updated "getBitcoinPrice.sh" to properly store results in "bitcoin_prices.txt"

--- getBitcoinPrice.sh
+++ getBitcoinPrice.sh
@@ -1,41 +1,27 @@
+#!/bin/bash
+
 url="https://www.coindesk.com/price/bitcoin/"
 content=$(curl -s "$url")
 
-# Pattern of the current price
-currentPricePattern='<span class="currency-pricestyles__Price-sc-1v249sx-0 fcfNRE">([0-9,\.]+)</span>'
+# Pattern for the Bitcoin price
+pricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
-# Find and clean the current price
-result=$(echo "$content" | grep -oP "$currentPricePattern" | sed -E 's/.*>([0-9,.]+)<.*/\1/')
+# Find and clean the Bitcoin price
+price=$(echo "$content" | grep -oP "$pricePattern" | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
 
-# Error handling for the current price
-if [ -z "$result" ]; then
-    echo "Error: Current Price not found or pattern mismatch"
-else
-    echo "Current Price:"
-    echo "$result"
-fi
+# Pattern for the 24H lowest rate
+lowestPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
+# Find and clean the 24H lowest price
+lowestPrice=$(echo "$content" | grep -oP "$lowestPricePattern" | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
 
-# Pattern for the 24H lowest and highest rates
-dayRatePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)<\/span>.*<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)<\/span>'
+# Pattern for the 24H highest rate
+highestPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
-# Find and clean the 24H lowest and highest rates
-result=$(echo "$content" | grep -oP "$dayRatePattern" | sed -E 's/.*\$([0-9,\.]+)<\/span>.*<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)<\/span>.*/\1\n\2/')
+# Find and clean the 24H highest price
+highestPrice=$(echo "$content" | grep -oP "$highestPricePattern" | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
 
-# Error handling for the 24H lowest and highest rates
-if [ -z "$result" ]; then
-    echo "Error: 24H Lowest and Highest Rates not found or pattern mismatch"
-else
-    prices=($result)
-    echo "24H Lowest Rrice:"
-    echo "${prices[0]}"
-    echo "24H Highest Rrice:"
-    echo "${prices[1]}"
-fi
-
-
-
-
-
-
-
+# Store the results in a text file
+echo "Bitcoin Price: $price" > /home/ripetomato/tracker/bitcoin_prices.txt
+echo "24H Lowest Price: $lowestPrice" >> /home/ripetomato/tracker/bitcoin_prices.txt
+echo "24H Highest Price: $highestPrice" >> /home/ripetomato/tracker/bitcoin_prices.txt
