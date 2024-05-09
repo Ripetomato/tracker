e7f5725b8fc1ea048717a2a3b2f9e6c2be691b2a
Ripetomato <169016177+Ripetomato@users.noreply.github.com>
2024-05-09 21:38:18 +0800

    Updated "getCryptoPrice.sh" to save the data to a database

--- getCryptoPrice.sh
+++ getCryptoPrice.sh
@@ -7,19 +7,19 @@ bitcoinContent=$(curl -s "$bitcoinUrl")
 # Pattern for the Bitcoin price
 bitcoinPricePattern='<span class="currency-pricestyles__Price-sc-1v249sx-0 fcfNRE">([^<]+)</span>'
 # Find and clean the Bitcoin price
-bitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$bitcoinPricePattern" | sed -E 's/<\/?span[^>]*>//g')
+bitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$bitcoinPricePattern" | sed -E 's/<\/?span[^>]*>//g' | tr -d ',')
 
 # Pattern for the 24H lowest Bitcoin price
 lowestBitcoinPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 # Find and clean the 24H lowest Bitcoin price
-lowestBitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$lowestBitcoinPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+lowestBitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$lowestBitcoinPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
 # Pattern for the 24H highest Bitcoin price
 highestBitcoinPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 # Find and clean the 24H highest Bitcoin price
-highestBitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$highestBitcoinPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+highestBitcoinPrice=$(echo "$bitcoinContent" | grep -oP "$highestBitcoinPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
-#============================================================================================================================================
+#====================================================================================================================================================================================================
 
 # Connect to the webpage with Ethereum prices
 ethereumUrl="https://www.coindesk.com/price/ethereum/"
@@ -28,19 +28,19 @@ ethereumContent=$(curl -s "$ethereumUrl")
 # Pattern for the Ethereum price
 ethereumPricePattern='<span class="currency-pricestyles__Price-sc-1v249sx-0 fcfNRE">([^<]+)</span>'
 # Find and clean the Ethereum price
-ethereumPrice=$(echo "$ethereumContent" | grep -oP "$ethereumPricePattern" | sed -E 's/<\/?span[^>]*>//g')
+ethereumPrice=$(echo "$ethereumContent" | grep -oP "$ethereumPricePattern" | sed -E 's/<\/?span[^>]*>//g' | tr -d ',')
 
 # Pattern for the 24H lowest Ethereum price
 lowestEthereumPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 # Find and clean the 24H lowest Ethereum price
-lowestEthereumPrice=$(echo "$ethereumContent" | grep -oP "$lowestEthereumPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+lowestEthereumPrice=$(echo "$ethereumContent" | grep -oP "$lowestEthereumPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
 # Pattern for the 24H highest Ethereum price
 highestEthereumPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 # Find and clean the 24H highest Ethereum price
-highestEthereumPrice=$(echo "$ethereumContent" | grep -oP "$highestEthereumPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+highestEthereumPrice=$(echo "$ethereumContent" | grep -oP "$highestEthereumPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
-#============================================================================================================================================
+#====================================================================================================================================================================================================
 
 # Connect to the webpage with Binance Coin prices
 binanceUrl="https://www.coindesk.com/price/binance-coin/?_gl=1*nqhlxd*_up*MQ..*_ga*NDk0NzUyMDE3LjE3MTUxNzExNDQ.*_ga_VM3STRYVN8*MTcxNTE3MTE0NC4xLjAuMTcxNTE3MTE0NC4wLjAuMTQzODYyMDUw"
@@ -50,21 +50,21 @@ binanceContent=$(curl -s "$binanceUrl")
 binancePricePattern='<span class="currency-pricestyles__Price-sc-1v249sx-0 fcfNRE">([^<]+)</span>'
 
 # Find and clean the Binance Coin price
-binancePrice=$(echo "$binanceContent" | grep -oP "$binancePricePattern" | sed -E 's/<\/?span[^>]*>//g')
+binancePrice=$(echo "$binanceContent" | grep -oP "$binancePricePattern" | sed -E 's/<\/?span[^>]*>//g' | tr -d ',')
 
 # Pattern for the 24H lowest Binance Coin price
 lowestBinancePricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
 # Find and clean the 24H lowest Binance Coin price
-lowestBinancePrice=$(echo "$binanceContent" | grep -oP "$lowestBinancePricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+lowestBinancePrice=$(echo "$binanceContent" | grep -oP "$lowestBinancePricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
 # Pattern for the 24H highest Binance Coin price
 highestBinancePricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
 # Find and clean the 24H highest Binance Coin price
-highestBinancePrice=$(echo "$binanceContent" | grep -oP "$highestBinancePricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+highestBinancePrice=$(echo "$binanceContent" | grep -oP "$highestBinancePricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
-#============================================================================================================================================
+#====================================================================================================================================================================================================
 
 # Connect to the webpage with Solana prices
 solanaUrl="https://www.coindesk.com/price/solana/?_gl=1*1w7qejl*_up*MQ..*_ga*NDk0NzUyMDE3LjE3MTUxNzExNDQ.*_ga_VM3STRYVN8*MTcxNTE3MTE0NC4xLjEuMTcxNTE3MTE2Ny4wLjAuMTQzODYyMDUw"
@@ -74,54 +74,43 @@ solanaContent=$(curl -s "$solanaUrl")
 solanaPricePattern='<span class="currency-pricestyles__Price-sc-1v249sx-0 fcfNRE">([^<]+)</span>'
 
 # Find and clean the Solana price
-solanaPrice=$(echo "$solanaContent" | grep -oP "$solanaPricePattern" | sed -E 's/<\/?span[^>]*>//g')
+solanaPrice=$(echo "$solanaContent" | grep -oP "$solanaPricePattern" | sed -E 's/<\/?span[^>]*>//g' | tr -d ',')
 
 # Pattern for the 24H lowest Solana price
 lowestSolanaPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
 # Find and clean the 24H lowest Solana price
-lowestSolanaPrice=$(echo "$solanaContent" | grep -oP "$lowestSolanaPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+lowestSolanaPrice=$(echo "$solanaContent" | grep -oP "$lowestSolanaPricePattern" | head -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
 # Pattern for the 24H highest Solana price
 highestSolanaPricePattern='<span class="typography__StyledTypography-sc-owin6q-0 dtjHgI">\$([0-9,\.]+)</span>'
 
 # Find and clean the 24H highest Solana price
-highestSolanaPrice=$(echo "$solanaContent" | grep -oP "$highestSolanaPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/')
+highestSolanaPrice=$(echo "$solanaContent" | grep -oP "$highestSolanaPricePattern" | tail -n 1 | sed -E 's/.*\$([0-9,\.]+)<.*/\1/' | tr -d ',')
 
-#============================================================================================================================================
+#====================================================================================================================================================================================================
 
-# Store the results in a text file
-echo "Bitcoin Price: $bitcoinPrice" > /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Lowest Bitcoin Price: $lowestBitcoinPrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Highest Bitcoin Price: $highestBitcoinPrice" >> /home/ripetomato/tracker/crypto_prices.txt
+# MySQL database details
+db_user="root"
+db_password="password"
+db_name="crypto_prices_db"
 
-echo "Ethereum Price: $ethereumPrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Lowest Ethereum Price: $lowestEthereumPrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Highest Ethereum Price: $highestEthereumPrice" >> /home/ripetomato/tracker/crypto_prices.txt
+# Insert Bitcoin prices into MySQL table
+mysql -u "$db_user" -p"$db_password" "$db_name" -e \
+"INSERT INTO prices (CurrencyName, Price, 24HLowestPrice, 24HHighestPrice) \
+VALUES ('Bitcoin', '$bitcoinPrice', '$lowestBitcoinPrice', '$highestBitcoinPrice');"
 
-echo "Binance Coin Price: $binancePrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Lowest Binance Coin Price: $lowestBinancePrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Highest Binance Coin Price: $highestBinancePrice" >> /home/ripetomato/tracker/crypto_prices.txt
+# Insert Ethereum prices into MySQL table
+mysql -u "$db_user" -p"$db_password" "$db_name" -e \
+"INSERT INTO prices (CurrencyName, Price, 24HLowestPrice, 24HHighestPrice) \
+VALUES ('Ethereum', '$ethereumPrice', '$lowestEthereumPrice', '$highestEthereumPrice');"
 
-echo "Solana Price: $solanaPrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Lowest Solana Price: $lowestSolanaPrice" >> /home/ripetomato/tracker/crypto_prices.txt
-echo "24H Highest Solana Price: $highestSolanaPrice" >> /home/ripetomato/tracker/crypto_prices.txt
+# Insert Binance Coin prices into MySQL table
+mysql -u "$db_user" -p"$db_password" "$db_name" -e \
+"INSERT INTO prices (CurrencyName, Price, 24HLowestPrice, 24HHighestPrice) \
+VALUES ('Binance Coin', '$binancePrice', '$lowestBinancePrice', '$highestBinancePrice');"
 
-: <<EOF
-# Show the results in the terminal (to check if the data extracted is correct)
-echo "Bitcoin Price: $bitcoinPrice"
-echo "24H Lowest Bitcoin Price: $lowestBitcoinPrice"
-echo "24H Highest Bitcoin Price: $highestBitcoinPrice"
-
-echo "Ethereum Price: $ethereumPrice"
-echo "24H Lowest Ethereum Price: $lowestEthereumPrice"
-echo "24H Highest Ethereum Price: $highestEthereumPrice"
-
-echo "Binance Coin Price: $binancePrice"
-echo "24H Lowest Binance Coin Price: $lowestBinancePrice"
-echo "24H Highest Binance Coin Price: $highestBinancePrice"
-
-echo "Solana Price: $solanaPrice"
-echo "24H Lowest Solana Price: $lowestSolanaPrice"
-echo "24H Highest Solana Price: $highestSolanaPrice"
-EOF
+# Insert Ethereum prices into MySQL table
+mysql -u "$db_user" -p"$db_password" "$db_name" -e \
+"INSERT INTO prices (CurrencyName, Price, 24HLowestPrice, 24HHighestPrice) \
+VALUES ('Solana', '$solanaPrice', '$lowestSolanaPrice', '$highestSolanaPrice');"
